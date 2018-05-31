53b1c256a21e72d2e7e021868f68909adb31b591
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-05-31 21:26:26 +0530

    Update syntax.php

--- Aniruddha/Lesson/syntax.php
+++ Aniruddha/Lesson/syntax.php
@@ -2,9 +2,9 @@
   // PHP support C like syntax, hence the comments.
   // PHP is case sensitive.
 
+  echo '<p>Hello, World!</p>';
   echo phpversion();
   echo phpinfo();
-  echo '<p>Hello, World!</p>';
 ?>
 
 
