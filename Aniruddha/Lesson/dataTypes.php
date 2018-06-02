c6dc313ba793999ff30d21aee064e1cbc4e193e7
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-06-03 02:05:14 +0530

    Update dataTypes.php

--- Aniruddha/Lesson/dataTypes.php
+++ Aniruddha/Lesson/dataTypes.php
@@ -23,5 +23,8 @@
 // - array|object
 // - void
 
+// To check return value use var_dump() function.
+// To check type use gettype() function.
+// To return type use is_type() functions (i.e is_int(), is_bool, etc).
 
 ?>
