25084d4cb45747332900453ebfc3eb51cc843801
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-06-02 22:07:30 +0530

    Update variables.php

--- Aniruddha/Lesson/variables.php
+++ Aniruddha/Lesson/variables.php
@@ -1,3 +1,12 @@
 <?php
-  
->
+    // In 'statically-typed' languages (i.e C/C++, Java), you must define the type of data that the variable will hold.
+    // PHP, on the other hand, is 'dynamically-typed' and variables are case sensitive.
+    // There is a difference b/w single-quote ('') and double-quote ("").
+
+    // Using double-quotes would tell the PHP parser that there may be more than just a string going in and to look for additional instructions.
+    $string1 = 'Hello, World!<br>';
+    $string2 = "Aniruddha $string1";
+    // Output: Aniruddha Hello, World!
+    
+    
+?>
