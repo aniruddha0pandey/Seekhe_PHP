6f366416ca42dda42b59c75a548fda1928725d09
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-06-03 01:58:08 +0530

    Update dataTypes.php

--- Aniruddha/Lesson/dataTypes.php
+++ Aniruddha/Lesson/dataTypes.php
@@ -1,19 +1,27 @@
 <?php
 
-Scalar types:
-- boolean
-- integer
-- float/double
-- string
+// Scalar types:
+// - boolean
+// - integer
+// - float/double
+// - string
   
-Compound types:
-- array
-- object
-- callable
-- iterable
+// Compound types:
+// - array
+// - object
+// - callable
+// - iterable
 
-Special types:
-- resource
-- NULL
+// Special types:
+// - resource
+// - NULL
+  
+// Pseudo types:
+// - mixed
+// - number
+// - callback/callable
+// - array|object
+// - void
+
 
 ?>
