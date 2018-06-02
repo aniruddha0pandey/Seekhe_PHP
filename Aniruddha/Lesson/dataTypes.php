c9bce15f74871adc6a32358c5c9428b741194f96
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-06-03 02:10:19 +0530

    Update dataTypes.php

--- Aniruddha/Lesson/dataTypes.php
+++ Aniruddha/Lesson/dataTypes.php
@@ -27,4 +27,15 @@
 // To check type use gettype() function.
 // To return type use is_type() functions (i.e is_int(), is_bool, etc).
 
+    var_dump((bool) "");           // false
+    var_dump((bool) 0.00);         // false
+    var_dump((bool) NULL);         // false
+    var_dump((bool) 1);            // true
+    var_dump((bool) -2);           // true
+    var_dump((bool) "foo");        // true
+    var_dump(0 == 'all');          // true
+    var_dump((string)0 == 'all');  // false
+    var_dump((bool) array(12));    // true
+    var_dump((bool) array());      // false
+
 ?>
