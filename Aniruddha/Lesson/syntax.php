41549c82249ae8b902f784e4f827a1e73fe56b8b
Aniruddha Pandey <31156696+aniruddha0pandey@users.noreply.github.com>
2018-06-02 22:43:18 +0530

    Update syntax.php

--- Aniruddha/Lesson/syntax.php
+++ Aniruddha/Lesson/syntax.php
@@ -1,4 +1,7 @@
 <?php
+    // When PHP parses a file, it looks for opening and closing tags, which are '<?php ... ?>'.
+    // Or the 'short_open_tag' (Default: 1) can be configured in the 'php.ini' directive.
+    // Or by '--enable-short-tags' command.
     // PHP support C like syntax, hence the comments.
     // PHP is case sensitive.
     // The 'echo' statment sends whatever text follows.
