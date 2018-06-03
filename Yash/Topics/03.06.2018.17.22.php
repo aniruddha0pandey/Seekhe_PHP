c0946da4fcc62a3cdbc61e77e19f2b6116e628cc
yashpathack <31958105+yashpathack@users.noreply.github.com>
2018-06-03 17:29:19 +0530

    Case Sensitive

--- Yash/Topics/03.06.2018.17.22.php
+++ Yash/Topics/03.06.2018.17.22.php
@@ -1,4 +1,12 @@
-,!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <body>
 <? php
+    ecHo "Here is something to be showcased";  
+    echo "Here is something to be showcased";
+    Echo "Here is something to be showcased";
+    ECHO "Here is something to be showcased";
+  
+  // There is no need to worry about writing a predefined tag of php in uppercase or lowercase.
+  // But  all variable names don't follow such fact. Each change in upper or lower case makes a variable discrete.
+?>
