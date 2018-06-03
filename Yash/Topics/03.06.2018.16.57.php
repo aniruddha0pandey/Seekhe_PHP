c6ef9c990ff5f8327e1ef0a18438983e8823465f
yashpathack <31958105+yashpathack@users.noreply.github.com>
2018-06-03 17:09:05 +0530

    Syntax

--- Yash/Topics/03.06.2018.16.57.php
+++ Yash/Topics/03.06.2018.16.57.php
@@ -1,1 +1,13 @@
+// The PHP script can be placed anywhere in the document. File extension: .php
+//echo is used to output a text. 
 
+<!DOCTYPE html>
+<html>
+  <body>
+    <h1> PHP page.</h1>
+    <?php
+    echo "Hello Galaxy!"; //Ends with a semicolon
+    ?>
+  </body>
+</html>
+    
