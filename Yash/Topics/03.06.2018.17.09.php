93aa26fd9bfc8306135efc49f530b0d735444824
yashpathack <31958105+yashpathack@users.noreply.github.com>
2018-06-03 17:22:31 +0530

    Comments

--- Yash/Topics/03.06.2018.17.09.php
+++ Yash/Topics/03.06.2018.17.09.php
@@ -1,1 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <?php
+    echo "Ways to comment in PHP";
+    // There are several ways to comment in PHP    
+    //This is a comment which is single line
+    
+    #This is a comment which is also single lined
+    
+    /*This is a multi lined comment
+    this can go
+    on and
+    on*/
+    
+   //One good way of using comments is to disabling a part of code.
+    $x = 5/*+15*/+5;
+    echo $x;
+    
+    ?>
+    
 
+  </body>
+</html>
