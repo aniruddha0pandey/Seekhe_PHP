8dafd456cc596f51b8ee3df03187f8e008f11844
yashpathack <31958105+yashpathack@users.noreply.github.com>
2018-06-03 19:29:34 +0530

    Create 03.06.2018.18.08.php

---
+++ Yash/Topics/03.06.2018.18.08.php
@@ -0,0 +1,10 @@
+<?php
+//Scopes of variables in PHP
+//Variables can be declared anywhere in the sript. There exist scopes of variables. Three scopes to be precise
+//Static
+//Global
+//Local
+
+  
+
+?>
