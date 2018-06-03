cecb3ff4d5ed351095e3e1c2b24e3d8ca59fc942
yashpathack <31958105+yashpathack@users.noreply.github.com>
2018-06-03 18:07:13 +0530

    Variables

---
+++ Yash/Topics/03.06.2018.18.06.php
@@ -0,0 +1,17 @@
+// In PHP a variable is initiated with a '$' Dollar sign. It's bucket to hold information.
+
+<?php
+  $eerie = "Did you hear Laurel?"; // Use quoted text to show a string of letters;
+  $t = 4;
+  $h = 34.3;
+  echo "Hi";
+// echo is an output variable.
+  echo "I have $t chocolates"; //Output: I have 4 chocolates.
+//______________________________________________________________________________________________________
+  echo "I have" .$t. "chocolates"; //Output: I have 4 chocolates
+//______________________________________________________________________________________________________
+// Arithmetic operations in PHP
+  echo $t + $34.3
+?>
+
+<!-- Note that in PHP you need not to worry about the data-type of the variable you're using. PHP identifies it automatically unlike C/C++.-->
