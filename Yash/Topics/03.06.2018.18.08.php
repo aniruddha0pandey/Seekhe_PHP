e666fba6bab2e31b2ae3e82fb9ef7057716cefa1
yashpathack <31958105+yashpathack@users.noreply.github.com>
2018-06-04 23:59:28 +0530

    Update 03.06.2018.18.08.php

--- Yash/Topics/03.06.2018.18.08.php
+++ Yash/Topics/03.06.2018.18.08.php
@@ -1,6 +1,6 @@
 <?php
 //Scopes of variables in PHP
-//Variables can be declared anywhere in the sript. There exist scopes of variables. Three scopes to be precise
+//Variables can be declared anywhere in the sript. There exist scopes of variables. Three scopes to be precise!
 //Static
 //Global
 //Local
